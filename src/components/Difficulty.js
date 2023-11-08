bc10bc336fb951ad4ecf1781d47ef1ad08e862ad
Jose Copeti <jrcopeti@gmail.com>
2023-11-08 12:50:32 +0100

    difficulty set up

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -3,6 +3,7 @@ function Difficulty({dispatch, difficulty}) {
     <div>
       <p>Please select a difficulty</p>
       <select onChange={(e)=> dispatch({type: "difficulty", payload: e.target.value})}>
+        <option value="">--Please choose an option--</option>
         <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">Hard</option>

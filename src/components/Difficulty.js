2f7bbac0bd1d81084c1eb15a4a038287372221fb
Jose Copeti <jrcopeti@gmail.com>
2023-11-09 09:29:18 +0100

    num questions set up

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -1,9 +1,12 @@
-function Difficulty({dispatch, difficulty}) {
+function Difficulty({ dispatch }) {
   return (
     <div>
-      <p>Please select a difficulty</p>
-      <select onChange={(e)=> dispatch({type: "difficulty", payload: e.target.value})}>
-        <option value="">--Please choose an option--</option>
+      <select
+        onChange={(e) =>
+          dispatch({ type: "difficulty", payload: e.target.value })
+        }
+      >
+        <option value="">--Please choose an DIFFICULTY--</option>
         <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">Hard</option>
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,8 @@
 function StartScreen({numQuestions, dispatch}) {
   return (
     <div>
-      <h2>Welcome to Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui" onClick={() => dispatch({type: "start"})}>Let's start</button>
+      <button className="btn btn-ui" onClick={() => dispatch({type: "start"})}>Start Quiz</button>
     </div>
   )
 }

c9753f8ce12038a8100632f01bdaafd7f6fb3723
Jose Copeti <jrcopeti@gmail.com>
2023-11-24 22:02:18 +0100

    fix numQuestions

--- src/components/NumQuestions/NumQuestions.js
+++ src/components/NumQuestions/NumQuestions.js
@@ -1,13 +1,6 @@
 import "./NumQuestions.css";
 
 function NumQuestions({ numQuestions, dispatch }) {
-  // const numberRef = useRef(null);
-  // const displayRef = useRef(null);
-
-  // function handleRangeChange() {
-  //   displayRef.current.textContent = numberRef.current.value;
-  // }
-
   return (
     <div className="category">
       <label>Select the number of questions</label>
@@ -25,27 +18,37 @@ function NumQuestions({ numQuestions, dispatch }) {
         ))}
       </select>
     </div>
-
-    // <div>
-    //   <input
-    //     ref={numberRef}
-    //     type="range"
-    //     min="3"
-    //     max="20"
-    //     step="1"
-    //     defaultValue="1"
-    //     onChange={handleRangeChange}
-    //   />
-    //   <span ref={displayRef}>1</span>
-    //   <button
-    //     onClick={() =>
-    //       dispatch({ type: "numQuestions", payload: numberRef.current.value })
-    //     }
-    //   >
-    //     Confirm
-    //   </button>
-    // </div>
   );
 }
+
+// If you want to use input range, you can do it like this:
+// import { useRef } from "react";
+
+// const numberRef = useRef(null);
+// const displayRef = useRef(null);
+
+// function handleRangeChange() {
+//   displayRef.current.textContent = numberRef.current.value;
+// }
+
+// <div>
+//   <input
+//     ref={numberRef}
+//     type="range"
+//     min="3"
+//     max="20"
+//     step="1"
+//     defaultValue="1"
+//     onChange={handleRangeChange}
+//   />
+//   <span ref={displayRef}>1</span>
+//   <button
+//     onClick={() =>
+//       dispatch({ type: "numQuestions", payload: numberRef.current.value })
+//     }
+//   >
+//     Confirm
+//   </button>
+// </div>
 
 export default NumQuestions;
